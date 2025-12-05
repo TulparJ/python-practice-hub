eb4ecd2d225569765fb3c7341b56b09e88980a69
Tulpar Janbolat <tulparjanbolat.1@gmail.com>
2025-12-04 21:18:11 -0500

    Create unit_converter.py

---
+++ unit_converter.py
@@ -0,0 +1,8 @@
+def km_to_miles(km):
+    return round(km * 0.621371, 2)
+
+def kg_to_lbs(kg):
+    return round(kg * 2.20462, 2)
+
+print("5 km =", km_to_miles(5), "miles")
+print("70 kg =", kg_to_lbs(70), "lbs")
