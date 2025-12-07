09d0b128eb248df3a64001157dd5eda4c94a2410
Tulpar Janbolat <tulparjanbolat.1@gmail.com>
2025-12-07 15:55:45 -0500

    Create simple_calculator.py

---
+++ simple_calculator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print("5 + 3 =", add(5, 3))
+print("10 - 4 =", subtract(10, 4))
+print("6 * 7 =", multiply(6, 7))
+print("20 / 5 =", divide(20, 5))
