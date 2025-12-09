ea8d5ca73d691fd8cd5fcfdf2148910653153bde
Tulpar Janbolat <tulparjanbolat.1@gmail.com>
2025-12-08 21:07:50 -0500

    Add daily affirmation generator

---
+++ daily_affirmation.py
@@ -0,0 +1,11 @@
+import random
+
+affirmations = [
+    "You’re leveling up every day.",
+    "Keep going, future legend.",
+    "You’re smarter than you think.",
+    "One grind at a time.",
+    "Focus now, flex later.",
+]
+
+print(random.choice(affirmations))
